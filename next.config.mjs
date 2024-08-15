78663fd96f2baceb443622e6411ae982fccb0757
是66呀 <2652915018@qq.com>
2024-08-15 20:51:47 +0800

    1

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-
-
+    images: {
+        loader: 'custom',
+        loaderFile: './my-loader.js',
+    },
 
 
 };
