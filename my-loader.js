ccdf41556c43f5ce76aaf1e0b60afa58135cf873
是66呀 <2652915018@qq.com>
2024-08-15 21:05:33 +0800

    1

--- my-loader.js
+++
@@ -1,6 +0,0 @@
-export default function cloudinaryLoader({ src, width, quality }) {
-    const params = ['f_auto', 'c_limit', `w_${width}`, `q_${quality || 'auto'}`]
-    return `https://res.cloudinary.com/demo/image/upload/${params.join(
-        ','
-    )}${src}`
-}
--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    basePath: '',
     output: 'export',
     images: {
         loader: 'custom',
-        loaderFile: './my-loader.js',
+        loaderFile: (function() {
+            const basePath = nextConfig.basePath; // 捕获basePath
+            return function Loader({ src, width, quality }) {
+                // 现在你可以使用basePath了
+                return `${basePath}/${src}?w=${width}&q=${quality || 'auto'}`;
+            };
+        })()
     },
 
 
+
 };
+
 
 export default nextConfig;
