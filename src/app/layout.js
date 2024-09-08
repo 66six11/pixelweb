33a3f091cb79e1c0c81d9b7cc12fe4d4490ad51d
是66呀 <2652915018@qq.com>
2024-09-09 00:58:43 +0800

    修正layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -17,14 +17,14 @@ export default function RootLayout({ children }) {
     <html lang="zh-cn" className={myFont.className}>
       <body className="bg-slate-100 dark:bg-slate-900 ">
         <Navbar />
-        
+        <ImageView>
         <div className=" fixed flex-col w-full h-full overflow-auto hiddenOverflow">
           <div className="w-4/5 mx-auto max-[450px]:px-1 grow text-black/60 dark:text-white/60 bg-white max-md:w-[95%] dark:bg-gray-800  px-[60px] pt-8  lg:pt-[5rem] max-lg:pb-[5rem] tracking-wider">
             {children}
             <div className="h-20" ></div>
           </div>
         </div>
-        
+        </ImageView>
       </body>
     </html>
   );
